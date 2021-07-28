3979b4c216d002c873458963d4e6227d27652019
Server Ve Skříni <serverveskrini@tobbox.home>
2021-07-28 09:41:49 +0200

    Added login and register form

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="login">
     <h1>Login</h1>
+
+    <form>
+      <div class="field">
+        <label class="label">Jméno a příjmení</label>
+        <div class="control">
+          <input class="input" type="text">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Heslo</label>
+        <div class="control">
+          <input class="input" type="password">
+        </div>
+      </div>
+    </form>
   </div>
 </template>
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="register">
     <h1>Register</h1>
+
+    <form>
+      <div class="field">
+        <label class="label">Jméno a příjmení</label>
+        <div class="control">
+          <input class="input" type="text">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Email</label>
+        <div class="control">
+          <input class="input" type="email" placeholder="např. alexsmith@gmail.com">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Heslo</label>
+        <div class="control">
+          <input class="input" type="password">
+        </div>
+      </div>
+    </form>
   </div>
 </template>
