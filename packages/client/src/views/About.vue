32060bac458ffb46c45e40ef0d6581cfcc78cfab
FilipKohlik <72147498+FilipKohlik@users.noreply.github.com>
2021-08-24 11:36:29 +0200

    Update About.vue

--- packages/client/src/views/About.vue
+++ packages/client/src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about">
     <h1>ABOUT</h1>
-    <a>Blue snake is a social media network for scuba divers made by two students as a school project at MENSA Gymnasium.
-    This project is programmed in vue.js and powered by netlify.</a>
+    <p>Blue snake is a social media network for scuba divers made by two students as a school project at MENSA Gymnasium.
+    This project is programmed in vue.js and powered by netlify.</p>
     <a href="https://github.com/TobAFilip/BlueSnake">Our github</a>
   </div>
 </template>
