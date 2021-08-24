87b03ac19cea6a0412de2f39e661eafebffb0066
FilipKohlik <72147498+FilipKohlik@users.noreply.github.com>
2021-08-24 10:15:49 +0200

    Update Home.vue

--- packages/client/src/views/Home.vue
+++ packages/client/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/BLUESNAKE_LOGO.png"style="border-radius: 500px;">
+    <img alt="Vue logo" src="../assets/BLUESNAKE_LOGO.png">
   </div>
 </template>
 
